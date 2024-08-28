e79872f284cd2ee911d8fc4682153126e8993865
bobby <sirbob12@gmail.com>
2024-08-28 16:44:34 +0100

    Initial commit of Django API

---
+++ myblog/datatimes/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+#from django.utils import timezone
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    writter =models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
