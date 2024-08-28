e07a5c63a853f28c1876366c3998d426de47eecc
bobby <sirbob12@gmail.com>
2024-08-28 16:44:34 +0100

    Initial commit of Django API

---
+++ myblog/datatimes/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','writter', 'created_at', 'updated_at')
+    search_fields = ('title', 'content')
+
+
